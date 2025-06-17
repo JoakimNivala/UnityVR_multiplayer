b3ecdba7962962f4aae8be2ec6958026cad998ea
JoakimNivala <100118821+JoakimNivala@users.noreply.github.com>
2025-06-17 09:30:05 +0300

    Some changes

---
+++ VR Multiplayer/Assets/ThingSound.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ThingSound : MonoBehaviour
+{
+    private AudioClip glorp;
+    [SerializeField] private AudioSource AudioSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        glorp = GetComponent<AudioClip>();
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Puck")
+        {
+            Debug.Log("??? LOLOOSADOLAOKSLAOSOALSOAS");
+            AudioSource.PlayOneShot(glorp);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
