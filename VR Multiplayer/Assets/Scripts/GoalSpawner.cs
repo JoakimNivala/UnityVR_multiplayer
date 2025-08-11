3e960af436ab7905665db97d498ef537e5f4cfa2
JoakimNivala <100118821+JoakimNivala@users.noreply.github.com>
2025-08-11 07:53:35 +0300

    test

--- VR Multiplayer/Assets/Scripts/GoalSpawner.cs
+++ VR Multiplayer/Assets/Scripts/GoalSpawner.cs
@@ -9,18 +9,14 @@ public class GoalSpawner : NetworkBehaviour
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
-        GoalLogicStarterRpc();
-        
-    }
-
-    [Rpc(SendTo.Server, RequireOwnership = false)]
-    private void GoalLogicStarterRpc(RpcParams rpcParams = default)
-    {
-        for (int i = 0; i < arrayObjects.Length; i++)
+        if (IsServer)
         {
-            var obj = Instantiate(arrayObjects[i], spawnPoints[i].transform.position, Quaternion.identity);
-            obj.GetComponent<NetworkObject>().SpawnWithOwnership(rpcParams.Receive.SenderClientId);
+            for (int i = 0; i < arrayObjects.Length; i++)
+            {
+                var obj = Instantiate(arrayObjects[i], spawnPoints[i].transform.position, Quaternion.identity);
+                obj.GetComponent<NetworkObject>().Spawn();
+            }
         }
     }
-   
+
 }
